3fbbc296abb25ff861e63f3130c6b42c0930fb2c
dawnpatrolmustaine <dawnpatrolmustaine@712268c9-429c-17e3-30f9-6b70f38146ca>
2012-03-07 21:01:42 +0000

    upload extractClimates function

---
+++ gap-analysis/original-version-phaseolus/001.extractClimates.R
@@ -0,0 +1,46 @@
+# /* This program creates a SWD (sampling with data) file for a sample file in order to produce for maxent input
+# /* Use this program to extract from bioclim datasets and produce a very reliable model and then you could project it
+# /* to any geographic area, resolution or timeslice you may desire.
+# 
+# /* Written by Julian Ramirez
+# /* CIAT, March 2012
+
+library(raster)
+
+#function to extract climate data
+extractClimates <- function(input_dir,sample_file,env_dir,env_prefix,
+                            env_ext,lonfield,latfield,taxfield,
+                            output_dir) {
+  occ <- read.csv(paste(input_dir,"/",sample_file, sep=""))
+  xy <- data.frame(X=occ[,"lonfield"],Y=occ[,"latfield"])
+  swd <- occ
+  
+  for (i in 1:19) {
+    cat("Reading environmental layer",i,"\n")
+    rs <- raster(paste(envdir,"/",env_prefix,env_ext,sep=""))
+    swd$NEW <- extract(rs,xy)
+    names(swd)[i+3] <- paste(prefix,i,sep="")
+  }
+  
+  #cleaning the dataset and writing a separate dataset for those records with any missing record
+  fun <- function(x) {
+    nas <- which(is.na(x))
+    if (length(nas)>0) {ok <- 0} else {ok <- 1}
+    return(ok)
+  }
+  
+  #get the cleaning field in the occurrence matrix
+  swdin <- swd[,4:22]
+  swd$IS_OK <- apply(swdin,1,fun)
+  
+  #write all the stuff
+  swd_wrong <- swd[which(swd$IS_OK==1),]
+  write.csv(swd_wrong,paste(output_dir,"/occurrences_swd_wrong.csv",sep=""),quote=F,row.names=F)
+  
+  swd_ok <- swd[which(swd$IS_OK==0),]
+  write.csv(swd_ok,paste(output_dir,"/occurrences_swd_ok.csv",sep=""),quote=F,row.names=F)
+  
+  swd_all <- swd; swd_all$IS_OK <- NULL
+  write.csv(swd_all,paste(output_dir,"/occurrences_swd_all.csv",sep=""),quote=F,row.names=F)
+}
+
