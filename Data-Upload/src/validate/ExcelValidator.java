cd00514be2a957e32d31dee3e9f9413846ac695a
htobon <htobon@712268c9-429c-17e3-30f9-6b70f38146ca>
2012-05-15 21:29:24 +0000

    removing excel columns after z.

--- Data-Upload/src/validate/ExcelValidator.java
+++ Data-Upload/src/validate/ExcelValidator.java
@@ -3,6 +3,7 @@ package validate;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
@@ -14,11 +15,27 @@ public class ExcelValidator {
 	
 	public static void main(String[] args) throws InvalidFormatException, IOException, ClassNotFoundException, SQLException {
 		PropertiesManager propManager = new PropertiesManager();
-		//ExcelFileManager fileManager = new ExcelFileManager(propManager);
-		//ArrayList<String> excelColumns = fileManager.getColumns(false);
+		ExcelFileManager fileManager = new ExcelFileManager(propManager);
+		ArrayList<String> excelColumns = fileManager.getColumns(false);
 		
-		MySQLDAOManager mysqlManager = new MySQLDAOManager(propManager);
-		ArrayList<String> mysqlColumns = mysqlManager.getColumns();
+		String name;
+		boolean startDeleting = false;
+		for(Iterator<String> i = excelColumns.iterator(); i.hasNext(); ) {
+			name = i.next();
+			if(startDeleting) {
+				i.remove();
+			} else if(name.equals("z")) {
+				i.remove();
+			}
+		}
+		
+		for(String s : excelColumns) {
+			System.out.println(s);
+		}
+		
+		
+		//MySQLDAOManager mysqlManager = new MySQLDAOManager(propManager);
+		//ArrayList<String> mysqlColumns = mysqlManager.getColumns();
 		
 		
 	}
